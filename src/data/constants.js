b53ea9ad20d7731ec2359c63a6d04701cb94b3a2
assiayussiri659 <ayushdubey2053@gmail.com>
2025-01-22 15:59:54 +0530

    m

--- src/data/constants.js
+++ src/data/constants.js
@@ -165,7 +165,7 @@ export const education = [
   },
   {
     id: 1,
-    img: "https://kpic.co.in/images/logo.jpg",
+    img: "https://pikbest.com/png-images/school-vector-design-logo-4_10739082.html",
     school: "Dr kp Jaiswal Inter college, Prayagraj",
     date: "Apr 2019 - Apr 2020",
     desc: "I completed my class 12 high school education at Dr kp Jaiswal Inter collegel, Prayagraj, where I studied Science Side.",
